adceda4458952b6105cd4d13aebb17bf153d196d
williamstudios <35510958+williamstudios@users.noreply.github.com>
2018-09-24 15:55:34 -0500

    conexion
    conexion

--- conect.php
+++ conect.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set("America/Mexico_City");
-$con = mysqli_connect("servidor1224.il.controladordns.com","dovesolu_william","mamajuju261289","dovesolu_besmart");
+$con = mysqli_connect("hv23svg078.neubox.net","mercadov_william","1Waterman.","mercadov_besmart");
 date_default_timezone_get();
 if (mysqli_connect_errno($con)) {
  echo "Error al conectar con la base de datos";
